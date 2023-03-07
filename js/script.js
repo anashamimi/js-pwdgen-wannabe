3e44cee52494a4a1497a938685c9de05d047a043
anashamimi <anashamimi2@gmail.com>
2023-03-07 18:16:12 +0100

    aggiunta controllo con for e else

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@ let nomeUtente = prompt('Inserisci il tuo nome');
 let cognomeUtente = prompt('Inserisci il tuo cognome');
 let coloreUtente = prompt('Inserisci un colore che preferisci');
 
-console.log(nomeUtente, cognomeUtente, coloreUtente);
+if (nomeUtente && cognomeUtente && coloreUtente) {
+    document.getElementById('password').innerHTML = `${nomeUtente+cognomeUtente+coloreUtente+num}`;
+    console.log(nomeUtente, cognomeUtente, coloreUtente);
+} 
 
-document.getElementById('password').innerHTML = `${nomeUtente+cognomeUtente+coloreUtente+num}`;
+else{
+    document.getElementById('password').innerHTML = `<h2>NON FARE IL FURBO CON ME INSERISCI VALORI VALIDI!!!</h2>`;
+}
+
+
+
+
