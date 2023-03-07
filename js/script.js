4b1de7a7c875c6fc7b5473c6ea41f114ae7502ad
anashamimi <anashamimi2@gmail.com>
2023-03-07 18:31:46 +0100

    aggiunta comando replace che toglie spazi inseriti dall'utente

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const num = 21;
 let nomeUtente = prompt('Inserisci il tuo nome');
+nomeUtente = nomeUtente.replace(/ /g, "");
 let cognomeUtente = prompt('Inserisci il tuo cognome');
+cognomeUtente = cognomeUtente.replace(/ /g, "");
 let coloreUtente = prompt('Inserisci un colore che preferisci');
+coloreUtente = coloreUtente.replace(/ /g, "");
 
 if (nomeUtente && cognomeUtente && coloreUtente) {
     document.getElementById('password').innerHTML = `${nomeUtente+cognomeUtente+coloreUtente+num}`;
@@ -11,6 +14,7 @@ if (nomeUtente && cognomeUtente && coloreUtente) {
 else{
     document.getElementById('password').innerHTML = `<h2>NON FARE IL FURBO CON ME INSERISCI VALORI VALIDI!!!</h2>`;
 }
+
 
 
 
